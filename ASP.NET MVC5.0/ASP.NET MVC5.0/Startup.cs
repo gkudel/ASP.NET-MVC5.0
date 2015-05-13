9879d8c03820fd3ccc2eb3356746cc7009b2be38
Grzegorz Kudlacz <gkudel@gmail.com>
2015-05-13 11:56:09 +0200

    Initi

---
+++ ASP.NET MVC5.0/ASP.NET MVC5.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC5._0.Startup))]
+namespace ASP.NET_MVC5._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
