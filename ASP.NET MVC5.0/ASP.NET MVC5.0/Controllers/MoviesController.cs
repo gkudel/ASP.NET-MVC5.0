b178957947e1cc440449cde2302a33ef57d4e771
Grzegorz Kudlacz <gkudel@gmail.com>
2015-05-17 21:15:53 +0200

    Finish

--- ASP.NET MVC5.0/ASP.NET MVC5.0/Controllers/MoviesController.cs
+++ ASP.NET MVC5.0/ASP.NET MVC5.0/Controllers/MoviesController.cs
@@ -61,7 +61,7 @@ namespace ASP.NET_MVC5._0.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price, Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -93,7 +93,7 @@ namespace ASP.NET_MVC5._0.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price, Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
